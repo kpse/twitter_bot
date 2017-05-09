dbe4ed0669672af9ce8f3d8dcc15428435c5cb2a
Suo Qin <kpsesuo@gmail.com>
2017-05-09 22:43:53 +1000

    twit it

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const bot = new Twit({
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000
   })
+
+
+bot.post('statuses/update', {status: 'hello world!'}, (err, data, response) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(`${data.text} was twitted`)
+  }
+})
